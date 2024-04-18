c5be94c39e36c77a509dfcdd57ddc44d080ab958
Anup kumar Ghose <anupghose97@gmail.com>
2024-04-18 12:44:30 +0530

    My second

---
+++ List.py
@@ -0,0 +1,147 @@
+#li=[89,90,76,56,45]
+#li.append(900)
+
+#print(li)
+
+
+'''li1=['A','B','C','D','E']
+li2=['F','G','H','k']
+
+#li1.extend(li2)
+
+#print(li1)
+
+
+#print(li2)'''
+
+
+
+#li=[10,21,29,30,32,13]
+
+
+
+
+'''di_abhinash={'Name':'Abhinash','Age':'26','Salary':89000,'Dept':'QA','Area':'Abhinash'}
+di=di_abhinash.copy()
+
+print(di)'''
+
+#di_abhinash={'Name':'Abhinash','Age':'26','Salary':89000,'Dept':'QA','Area':'Abhinash'}
+
+#di_abhinash.setdefault('State','Odisha')
+
+#print(di_abhinash)
+
+
+'''di_abhinash.update({'State':'Odisha','Pin':756036,'post':'Dehurda'})
+
+print(di_abhinash)
+'''
+
+#li=[12,45,78,900,67,98,56,45,456,908,]
+
+#for i in li:
+     #if i%2!= 0:
+         #print(i)
+
+
+'''li=[67,90,60,'7908',9876,'njhhy','78906',567,'srs']
+str_li=[]
+for i in li:
+    if type(i)==str:
+        str_li.append(i)
+
+
+print(str_li)'''
+
+'''li=[10,20,30,40]
+sum=0
+for i in li:
+    sum=sum+i
+
+print(sum)'''
+
+#li=[10,20,30,40]
+
+#mult=1
+#for i in li:
+ #   mult=mult*1
+
+#print(mult)
+
+#str='i love india, india is best in the world'
+#li=[]
+#for i in str:
+    #if i =="o":
+      # li.append(i)
+
+#print(len(li))
+
+
+#di={'Name':'Utkal','Age':26,'Salary':90000}
+#for k,v in di.items():
+
+ #   print(k,v)
+
+#fib_series = [0,1]
+#num = 10
+#while len(fib_series)<=10:
+ #   new_num = fib_series[-1] + fib_series[-2]
+  #  fib_series.append(new_num)
+
+#print(fib_series)
+
+#num = 1
+#sum = 0
+#while num<=20:
+ #   sum = sum+num
+  #  num = num+1
+
+#print(sum)
+
+##new_str = ''
+
+#for i in str:
+#     new_str = i+new_str
+
+#print(new_str)
+
+#def add(a,b):
+ #   sum = a+b
+  #  if sum%2!=0:
+   #     def cu(sum):
+    #        cube =sum**2
+     #       return cube
+      #  return cu(sum)
+
+
+#print(add(10,20))
+
+#def decore(add):
+ #   def even_or_odd(a,b):
+  #      result = add(a,b)
+   #     if int(result)%2==0:
+    #        print(f'The result {result} is even')
+     #   else:
+      #      print(f'The result {result} is odd')
+
+    #return even_or_odd
+
+
+
+#@decore
+#def add(a,b):
+ #   result = str(int(a)+int(b))
+  #  return result
+
+
+# print(add(a=150,b=160))
+
+
+
+
+
+
+
+
+
