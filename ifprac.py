b5019a7d787c1e784732d9e3fb0f6df914b18351
Anup kumar Ghose <anupghose97@gmail.com>
2024-04-18 12:33:28 +0530

    My first code

---
+++ ifprac.py
@@ -0,0 +1,114 @@
+'''n = input("Enter a number:-") n: 6
+n = int(n)
+
+
+
+if  n % 2 == 0:
+    print("Number is even")
+
+else:
+    print("Number is odd")'''
+
+
+
+
+
+
+# i''''ndian = ["samosa","daal","naan"]
+# chinese = ["egg role","pot sticker","fried rice"]
+# italian = ["pizza","pasta","risotto"]
+#
+#
+# var = int(input("enter how much start you want"))
+#
+# '''for i in range(n):
+#     if j in range(n-1)'''
+#
+#
+# li = [12,13,11,15,10,26,12]'''
+#
+# '''for i in range(n):
+#     for j in range(0,j-n-1):
+#         if li[j]<li[j+1]:
+#             li[j],li[j+1] == li[j+1],li[j]
+#
+#
+# print(li)'''
+
+
+# li =[89,74,36,78,89,96,90,86]
+#
+#
+# max_val = float('-inf')
+# sec_max_val = float('-inf')
+# third_max_val =float('-inf')
+#
+#
+# for i in li:
+#     if i>max_val:
+#         third_max_val = sec_max_val
+#         sec_max_val = max_val
+#         max_val = i
+#
+#     elif i>sec_max_val and i!=max_val:
+#         third_max_val = sec_max_val
+#         sec_max_val = i
+# print(max_val,sec_max_val,third_max_val)
+
+
+
+
+#import pandas as pd
+#print(dir(pandas))
+
+
+#  import pandas
+#
+#
+#   data = {
+#    'Name': ['Gaurav','Saurav','lalit','Bishnu'],
+#    'Age' : [27,25,28,26],
+#    'City': ['Mumbai','Pune','Delhi','Chennai']
+#  }
+#
+#  df = pandas.DataFrame(data)
+# #  #print(df)
+#  df.to_excel('output.xlsx',index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
